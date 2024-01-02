ec3169a49250f87c2c5fdd2d966ba5a97c36f663
Borges <sdafdsf@.asdsa>
2024-01-02 14:28:42 -0300

    trying mono repository

---
+++ LearningIOS/UnitConversions/UnitConversions/UnitConversionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConversionsApp.swift
+//  UnitConversions
+//
+//  Created by paulo borges on 02/01/24.
+//
+
+import SwiftUI
+
+@main
+struct UnitConversionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
