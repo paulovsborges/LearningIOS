130abcf9348e28e03518bdc2fc9413d3f19f19e8
Borges <sdafdsf@.asdsa>
2024-01-02 18:24:32 -0300

    adding initial files for the guess the game project

---
+++ LearningIOS/GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by paulo borges on 02/01/24.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
