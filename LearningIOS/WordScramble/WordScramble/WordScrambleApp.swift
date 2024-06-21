2e86489fd259375f984adc186632c16abc960d85
pvsb <pvsbdev@gmail.com>
2024-06-21 16:09:19 -0300

    feat: adding word scramble project

---
+++ LearningIOS/WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Borges on 21/06/24.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
