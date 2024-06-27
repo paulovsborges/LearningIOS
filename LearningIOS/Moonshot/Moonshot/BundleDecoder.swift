a291a6d19635782689440a8ab23c6f130d1f8f8c
pvsb <pvsbdev@gmail.com>
2024-06-27 09:56:36 -0300

    feat: adding moonshot app

---
+++ LearningIOS/Moonshot/Moonshot/BundleDecoder.swift
@@ -0,0 +1,40 @@
+//
+//  BundleDecoder.swift
+//  Mooshot
+//
+//  Created by Borges on 27/06/24.
+//
+
+import Foundation
+
+extension Bundle{
+    func decode<T: Codable>(_ file: String) -> T{
+        guard let url = self.url(forResource: file, withExtension: nil)else {
+            fatalError("Could not locate file \(file)")
+        }
+        
+        guard let data = try? Data(contentsOf: url)else {
+            fatalError("Could not load file \(file)")
+        }
+        
+        let decoder = JSONDecoder()
+        
+        guard let loaded = try? decoder.decode(T.self, from: data) else{
+            fatalError("Could not decode file \(file)")
+        }
+        
+        do {
+            return try decoder.decode(T.self, from: data)
+        } catch DecodingError.keyNotFound(let key, let context) {
+            fatalError("Failed to decode \(file) due to missing key \(key.stringValue) -\(context.debugDescription)")
+        }catch DecodingError.typeMismatch(_, let context) {
+            fatalError("Failed to decode \(file) due to type mismatch - \(context.debugDescription)")
+        }catch DecodingError.valueNotFound(let type, let context) {
+            fatalError("Failed to decode \(file) due to missing \(type) value -\(context.debugDescription)")
+        }catch DecodingError.dataCorrupted (_) {
+            fatalError("Failed to decode \(file) because it appears to be invalid JSON.")
+        }catch {
+            fatalError("Failed")
+        }
+    }
+}
