ecb84496d81bc55af964d14f00a39ba78d03afa8
pvsb <pvsbdev@gmail.com>
2024-06-28 08:02:08 -0300

    feat: adding navigation app

---
+++ LearningIOS/Navigation/Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Borges on 28/06/24.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
