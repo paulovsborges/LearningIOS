d103144f25ddcd9e72118a281e3b51240e62fecd
pvsb <pvsbdev@gmail.com>
2025-02-27 12:56:31 -0300

    feat: adding learn ui kit and maps

--- LearningIOS/maps uikit/maps uikit/ViewController.swift
+++ LearningIOS/maps uikit/maps uikit/ViewController.swift
@@ -5,15 +5,70 @@
 //  Created by Borges on 27/02/25.
 //
 
+import MapKit
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let london = Capital(
+            title: "London",
+            coordinate: CLLocationCoordinate2D(
+                latitude: 51.507222,
+                longitude: -0.1275
+            ),
+            info: "Home of the 2012 summe olympics"
+        )
+        
+        let oslo = Capital(
+            title: "Oslo",
+            coordinate: CLLocationCoordinate2D(
+                latitude: 59.95,
+                longitude: 10.75
+            ),
+            info: "Founded over a thousand years ago"
+        )
+        
+        let paris = Capital(
+            title: "Paris",
+            coordinate: CLLocationCoordinate2D(
+                latitude: 48.8567,
+                longitude: 2.3508
+            ),
+            info: "Often called the City of Light"
+        )
+        
+        let rome = Capital(
+            title: "Rome",
+            coordinate: CLLocationCoordinate2D(
+                latitude: 41.9,
+                longitude: 12.5
+            ),
+            info: "Has a whole country inside it"
+        )
+        
+        let washington = Capital(
+            title: "Washington DC",
+            coordinate: CLLocationCoordinate2D(
+                latitude: 38.89111,
+                longitude: -77.036667
+            ),
+            info: "Named after George himself"
+        )
+        
+        /**
+        Annotations can be added to the map view one at a time or all at once with addAnnotations
+        mapView.addAnnotation(london)
+        mapView.addAnnotation(oslo)
+        mapView.addAnnotation(paris)
+        mapView.addAnnotation(rome)
+        mapView.addAnnotation(washington)
+         */
+        mapView.addAnnotations([london, oslo, paris, rome, washington])
     }
-
-
 }
 
